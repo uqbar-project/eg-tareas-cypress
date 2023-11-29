14c5f6e10c441b99729f7fedf879737f574c4544
starg09 <ultraguille2006@gmail.com>
2023-11-29 13:27:36 -0300

    Agregar custom commands

--- cypress/e2e/tareaCicloFeliz.cy.js
+++ cypress/e2e/tareaCicloFeliz.cy.js
@@ -1,51 +1,64 @@
 /// <reference types="cypress" />
 
-describe('template spec', () => {
+describe("template spec", () => {
+  const URL_BASE_BACKEND = "http://localhost:9000";
 
-  const URL_BASE_BACKEND = "http://localhost:9000"
+  beforeEach(() => {
+    cy.request("PUT", URL_BASE_BACKEND + "/reset/tareas");
+    cy.visit("http://localhost:4200/");
+  });
 
-  beforeEach(() =>{
-    cy.request("PUT", URL_BASE_BACKEND + "/reset/tareas")
-    cy.visit("http://localhost:4200/")
-  })
+  it("crea una tarea", () => {
+    const miTarea = {
+      descripcion: "Algo3: Preparar la clase de E2E Testing",
+      iteracion: "Cursada 2023",
+      asignatario: "Juan Contardo",
+    };
 
-  it('crea una tarea', () => {
+    cy.intercept({ method: "GET", url: URL_BASE_BACKEND + "/usuarios" }).as(
+      "getUsuarios"
+    );
 
-    cy.intercept({ method: 'GET', url: URL_BASE_BACKEND + "/usuarios" })
-    .as("getUsuarios")
+    cy.intercept({ method: "GET", url: "http://localhost:9000/tareas" }).as(
+      "getTareas"
+    );
 
-    cy.intercept({ method: 'GET', url: "http://localhost:9000/tareas" })
-    .as("getTareas")
+    cy.get("[data-testid='nueva-tarea']").click();
 
-    const descTarea =  "Algo3: Clase E2E"
+    cy.wait("@getUsuarios");
 
-    cy.get("[data-testid='nueva-tarea']").click()
+    cy.get('[data-testid="descripcion"').type(miTarea.descripcion);
 
-    cy.wait("@getUsuarios")
+    cy.get('[data-testid="iteracion"').type(miTarea.iteracion);
 
-    cy.get('[data-testid="descripcion"').type(descTarea)
+    cy.findByTestId("asignatario").select(miTarea.asignatario);
 
-    cy.get('[data-testid="iteracion"').type("Cursada 2023")
+    cy.get('input[placeholder="Fecha de inicio de la tarea"]').click();
 
-    cy.findByTestId('asignatario').select("Juan Contardo")
+    cy.get(".dp-calendar-wrapper").get(".dp-calendar-day").first().click();
 
-    cy.get('input[placeholder="Fecha de inicio de la tarea"]').click()
+    // cy.pause()
 
-    cy.get(".dp-calendar-wrapper").get(".dp-calendar-day").first().click()
+    cy.contains("Guardar").click();
 
-    cy.pause()
+    cy.wait("@getTareas");
 
-    cy.contains("Guardar").click()
-
-    cy.wait("@getTareas")
-
-    cy.get('[data-testid="fila-tarea"]').last().as("tareaCreada")
+    cy.get('[data-testid="fila-tarea"]').last().as("tareaCreada");
 
     cy.get("@tareaCreada")
-      .should("contain.text", descTarea)
+      .findDescripcion()
+      .should("contain.text", miTarea.descripcion);
 
     cy.get("@tareaCreada")
-      .find('[data-testid^="asignatario"]')
-      .should("contains.text", "Juan Contardo")
-  })
-})
+      .findIteracion()
+      .should("contains.text", miTarea.iteracion);
+      
+    cy.get("@tareaCreada")
+      .findAsignatario()
+      .should("contains.text", miTarea.asignatario);
+
+    cy.get("@tareaCreada").findCumplirButton().should("exist");
+    cy.get("@tareaCreada").findAsignarButton().should("exist");
+    cy.get("@tareaCreada").findDesasignarButton().should("exist");
+  });
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -26,3 +26,28 @@
 
 import '@testing-library/cypress/add-commands'
 
+Cypress.Commands.add('findDescripcion', { prevSubject: 'element'}, (subject) =>  subject.find('[data-testid^="descripcion_"]') )
+Cypress.Commands.add('findAsignatario', { prevSubject: 'element'}, (subject) =>  subject.find('[data-testid^="asignatario_"]') )
+Cypress.Commands.add('findFecha', { prevSubject: 'element'}, (subject) =>  subject.find('[data-testid^="fecha_"]') )
+Cypress.Commands.add('findIteracion', { prevSubject: 'element'}, (subject) =>  subject.find('[data-testid^="iteracion_"]') )
+Cypress.Commands.add('findCumplirButton', { prevSubject: 'element'}, (subject) =>  subject.find('[data-testid^="cumplir_"]') )
+Cypress.Commands.add('findAsignarButton', { prevSubject: 'element'}, (subject) =>  subject.find('[data-testid^="asignar_"]') )
+Cypress.Commands.add('findDesasignarButton', { prevSubject: 'element'}, (subject) =>  subject.find('[data-testid^="desasignar_"]') )
+
+Cypress.Commands.add('esMiTareaRecienCreada', { prevSubject: true}, (subject, miTarea) => {
+    subject
+        .findDescripcion()
+        .should("contain.text", miTarea.descripcion);
+
+    subject
+      .findAsignatario()
+      .should("contains.text", miTarea.asignatario);
+
+    subject
+      .findIteracion()
+      .should("contains.text", miTarea.iteracion);
+
+    subject.findCumplirButton().should("exist");
+    subject.findAsignarButton().should("exist");
+    subject.findDesasignarButton().should("exist");
+} )
