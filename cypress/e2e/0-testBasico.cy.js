ed74544fd39c9a7f0ab54956ae0785def36c0cd6
fdodino <fernando.dodino@gmail.com>
2024-11-20 21:43:47 -0300

    First basic test using todo list app

--- cypress/e2e/0-testBasico.cy.js
+++ cypress/e2e/0-testBasico.cy.js
@@ -1,17 +1,27 @@
 describe('My First Test', () => {
   it('Gets, types and asserts', () => {
-    cy.visit('https://example.cypress.io')
+    // Visito la página
+    cy.visit('https://example.cypress.io/todo')
 
-    cy.contains('type').click()
+    // Valido estado inicial
+    cy.get('.todo-list').children().as('items')
+    cy.get('@items').should('have.length', 2) // o podría hacerlo más resiliente y capturar el valor
 
-    // Should be on a new URL which
-    // includes '/commands/actions'
-    cy.url().should('include', '/commands/actions')
+    // Creo un nuevo ítem
+    const itemDescription = 'Learn cypress'
+    cy.get('[data-test="new-todo"]').type(`${itemDescription}\n`)
+    cy.get('@items').should('have.length', 3)
+    cy.get('@items').should('contain', itemDescription)
+    
+    // Marco el último ítem como cumplido
+    cy.get('@items').children().last().as('lastItem')
+    cy.get('@lastItem').find('.toggle').click()
 
-    // Get an input, type into it
-    cy.get('.action-email').type('fake@email.com')
-
-    //  Verify that the value has been updated
-    cy.get('.action-email').should('have.value', 'fake@email.com')
+    // Activo el filtro de solo activos y verifico que desaparece un ítem
+    // test más exhaustivo => chequeo que no esté itemDescription
+    // test más resiliente => asumo que es alguno de los ítems (qué pasa si se agrega
+    // cada elemento nuevo al principio?)
+    cy.get('[href="#/active"]').click()
+    cy.get('@items').should('have.length', 2)
   })
 })
