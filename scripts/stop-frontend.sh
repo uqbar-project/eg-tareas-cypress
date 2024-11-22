9c71b5a46bfd475388418dffe7ea38ad863bb83e
Fernando Dodino <fernando.dodino@gmail.com>
2024-11-22 00:17:58 -0300

    testing angular-http-server

--- scripts/stop-frontend.sh
+++ scripts/stop-frontend.sh
@@ -1,1 +1,2 @@
-kill -9 $(ps aux | grep eg-tareas-angular | awk 'NR==1{print $2}')
+# kill -9 $(ps aux | grep eg-tareas-angular | awk 'NR==1{print $2}')
+kill -9 $(ps aux | grep angular-http-server | awk 'NR==1{print $2}')
