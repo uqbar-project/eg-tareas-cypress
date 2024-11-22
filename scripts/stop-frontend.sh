66778a2415ce389ef389158f974ccf410d804bfe
Fernando Dodino <fernando.dodino@gmail.com>
2024-11-22 00:37:59 -0300

    Using retry

--- scripts/stop-frontend.sh
+++ scripts/stop-frontend.sh
@@ -1,2 +1,1 @@
-# kill -9 $(ps aux | grep eg-tareas-angular | awk 'NR==1{print $2}')
-kill -9 $(ps aux | grep angular-http-server | awk 'NR==1{print $2}')
+kill -9 $(ps aux | grep 'ng serve' | awk 'NR==1{print $2}')
