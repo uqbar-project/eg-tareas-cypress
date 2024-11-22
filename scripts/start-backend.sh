c53d2e8a290686a571682d3dfe835a78712f78f6
fdodino <fernando.dodino@gmail.com>
2024-11-22 19:10:17 -0300

    testing separate scripts

--- scripts/start-backend.sh
+++ scripts/start-backend.sh
@@ -1,6 +1,1 @@
-if [ ! -d eg-tareas-springboot-kotlin ] ; then
-  git clone https://github.com/uqbar-project/eg-tareas-springboot-kotlin
-fi
-cd eg-tareas-springboot-kotlin
-git pull
 nohup ./gradlew bootRun > output.log 2>&1 &
--- scripts/start-frontend.sh
+++ scripts/start-frontend.sh
@@ -1,8 +1,2 @@
-if [ ! -d eg-tareas-angular ] ; then
-  git clone https://github.com/uqbar-project/eg-tareas-angular
-fi
-cd eg-tareas-angular
-git pull
-npm install
 # nohup angular-http-server --silent --path ./src -p 4200 > output-frontend.log 2>&1 &
 nohup npm run start > output-frontend.log 2>&1 &
